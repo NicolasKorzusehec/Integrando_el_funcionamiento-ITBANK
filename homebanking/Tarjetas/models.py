a8431042bac89535e9733c70da5369577c400914
Elias Upstein <eliasupstein@gmail.com>
2022-08-12 23:49:12 -0300

    Incorporación de base de datos y modelos

--- homebanking/Tarjetas/models.py
+++ homebanking/Tarjetas/models.py
@@ -1,3 +1,32 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Tarjeta(models.Model):
+    card_id = models.AutoField(primary_key=True)
+    number_card = models.CharField(unique=True, max_length=200)
+    cvv = models.IntegerField()
+    issue_date = models.TextField()
+    exp_date = models.TextField()
+    type_card = models.TextField()
+    customer = models.ForeignKey('Cliente', models.DO_NOTHING)
+    brand = models.ForeignKey('MarcaTarjeta', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tarjeta'
+
+
+class MarcaTarjeta(models.Model):
+    brand_id = models.AutoField(primary_key=True)
+    brand_name = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'marca_tarjeta'
--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'homebanking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'BD_ampliada.db',
     }
 }
 
