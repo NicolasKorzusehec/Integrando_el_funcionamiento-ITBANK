02ad42734dad4921867d8d7c58a3dfcac43720e6
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-14 13:49:04 -0300

    settings.py: cambio lenguaje, agrego redirect del login/logout y path de la carpeta media

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             'Clientes/templates/Clientes',
-            'Login/templates/Login'
+            'Login/templates/Login'                                             #### analizable
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -129,3 +129,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+# Media files
+MEDIA_URL = '/media/'    #carpeta interna donde se encuentran los archivos
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  #URL externa
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# Redirect to home URL after logout (Default redirects to /accounts/profile/)
+LOGOUT_REDIRECT_URL = '/'
