125ddffc9104ae13bf343e48fd18d58541960612
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-17 00:45:46 -0300

    Vinculo modelo de Cuenta a la pestaña de admin propuesta por django

--- homebanking/Cuentas/admin.py
+++ homebanking/Cuentas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Cuenta
 # Register your models here.
+class PrestamoAdmin (admin.ModelAdmin):
+    readonly_fields= ()
+admin.site.register(Cuenta)
