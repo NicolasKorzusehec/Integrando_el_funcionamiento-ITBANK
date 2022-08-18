49248410e020496d67beecf2c013969cdd6c741a
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-18 02:14:15 -0300

    Solucionado! Elimine el segundo parametro (UserAdmin)del registro en el admin.py. Al instante comenzo a figurar en el modelo Usuarios dentro del administrador los nuevos campos

--- homebanking/Login/admin.py
+++ homebanking/Login/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib.auth.admin import UserAdmin
+#from django.contrib.auth.admin import UserAdmin
 from .models import UsuarioCustom
 
-admin.site.register(UsuarioCustom, UserAdmin)
+#admin.site.register(UsuarioCustom, UserAdmin)
+
+
+admin.site.register(UsuarioCustom)
