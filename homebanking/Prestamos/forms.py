dcac269205b0aa06ad99f5eb1e201a2b929c061c
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-20 19:23:55 -0300

    Armando el formulario de solicitud de prestamos, hay que definir la cuestion del monto de preaprobacion

--- homebanking/Prestamos/forms.py
+++ homebanking/Prestamos/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
+from .models import TipoPrestamo
+
 class solicitudPrestamo(forms.Form):
-    tipoPrestamo = forms.CharField(label="Tipo  ", required=True)
-    fechaInicio = forms.DecimalField(label="fecha de inicio",required=True,widget= forms.DateTimeInput())
+    tipoprestamo = TipoPrestamo.objects.all()
+    tipos = []
+    pos = 1
+    for tipo in tipoprestamo: 
+        tipos.append((pos, tipo.loan_name))
+        pos += 1 
+
+    loan_type= forms.IntegerField(label='Que tipo de prestamo desea?', required=True, widget=forms.Select(choices=tipos))
+
+    fechaInicio = forms.DateField(label="Fecha de inicio",required=True,widget= forms.DateTimeInput())
     monto = forms.DecimalField(label="Monto solicitado ", required=True)
