705f7f49181f2896cedfe641ff4d8e00904b383b
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-08-24 22:17:25 -0300

    Creo tabla tipocuenta La impacto y valido las foreign keys Se ven correctamente desde djangoadmin

--- homebanking/Cuentas/admin.py
+++ homebanking/Cuentas/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Cuenta, Movimientos
+from .models import Cuenta, Movimientos, TipoCuenta
 # Register your models here.
 class CuentaAdmin (admin.ModelAdmin):
     readonly_fields= ()
 admin.site.register(Cuenta)
+admin.site.register(TipoCuenta)
 admin.site.register(Movimientos) 
